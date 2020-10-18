18d2c95f21ee322ab981c9a02d638957c424373a
Your Name <smileynanda99@gmail.com>
2020-10-18 21:40:42 +0530

    add view-profile route and view, 404page

---
+++ routes/view.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const {  ensureAuthenticated} = require('../config/auth');
+
+router.get('/:username',ensureAuthenticated, async function(req, res) {
+    const {username } = req.params;
+    await User.findOne({username},async (err,profile)=>{
+        if(!err){
+          if(profile){
+             res.render('view-profile',{user:req.user, profile:profile});
+          }else{
+            res.render('404page');
+          }
+        } else{
+         console.log("somthing went wrong");
+       }
+     })
+});
+
+
+
+module.exports = router;
