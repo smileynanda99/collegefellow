930302dd5624f93a3df4704f68099149345d4f09
Your Name <smileynanda99@gmail.com>
2020-11-21 21:41:41 +0530

    fix follow list follow unfollow error removing <a> tag from btn

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const session =require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const flash = require('connect-flash');
 const server = require('http').createServer(app);
@@ -16,46 +15,8 @@ const io = require('socket.io').listen(server);
 //import model
 const User = require('./models/user');
 const {ChatRoom, ChatMsg }= require('./models/chatRoom');
-// require('./config/passport-config')(passport);
-//passport-config
-passport.use(
-  new LocalStrategy({ usernameField: 'collegeEmail' }, async(collegeEmail, password, done) => {
-    // Match user
-   await User.findOne({
-      collegeEmail: collegeEmail
-    }).then(user => {
-      if (!user) {
-        return done(null, false, { message: 'That email is not registered' });
-      } else{
-        if(!user.status){
-          return done(null, false, { message: 'Please verify your email.' });
-        }
-        bcrypt.compare(password, user.password, (err,result)=>{
-          if(err) {
-              return done(err);
-          }else{
-              if(result === true){
-                  //login
-                  return done(null, user);
-              } else{
-                  return done(null, false, { message: 'Password incorrect' });
-              }
-          }
-        });
-      }
-    });
-  })
-);
+require('./config/passport-config')(passport);
 
-passport.serializeUser(function(user, done) {
- return done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    return done(err, user);
-  });
-});
 //define port
 const PORT = process.env.PORT || 3000;
 
